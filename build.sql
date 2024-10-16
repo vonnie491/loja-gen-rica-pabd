e25f4cbd3db2fc126c50ec5067a3870830292899
Anderson Gleidson dos Santos Carvalho <20221214010025@ifrn.local>
2024-10-16 14:20:30 -0300

    acessando o banco de dados

---
+++ build.sql
@@ -0,0 +1,6 @@
+CREATE TABLE usuarios(
+	nome varchar(20) PRIMARY KEY,
+	senha varchar(12),
+	saldo float
+)
+
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import eel
 import psycopg2
 
-bancoDeDados = psycopg2.connect(dbname='lolja',user='postgres',password='pabd',host='localhost',port=5432)
+bancoDeDados = psycopg2.connect(dbname='loja',user='postgres',password='pabd',host='localhost',port=5432)
+bancoDeDados.autocommit = True
 cursor = bancoDeDados.cursor()
 
 @eel.expose
@@ -10,20 +11,24 @@ def login(nome,senha):
     listaUsuarios = cursor.fetchall()
 
     for usuario in listaUsuarios:
-        if usuario.nome == nome and usuario.senha == senha:
+        if usuario[0] == nome and usuario[1] == senha:
             return 'loja.html'
-    return '#'
     
-def conta(nome, senha):
-	cursor.execute('SELECT * FROM usuarios')
+@eel.expose
+def criarConta(nome,senha):
+    if nome == '' or senha == '':
+        return 'Nome ou Senha vazios'
+    if len(nome) < 5 or len(senha) < 5:
+        return 'Nome ou Senha muito curtos'
+    
+    cursor.execute('SELECT * FROM usuarios')
     listaUsuarios = cursor.fetchall()
 
-    for usuario in listaUsuarios:
-        if usuario.nome == nome and usuario.senha == senha:
-        	return '#'
-        else:
-        	cursor.execute("INSERT INTO conta (nome, senha) VALUES (%s, %s)"(usuario.nome, usuario.senha))
+    for usuario in listaUsuarios:#Não deixa criar usuários com mesmo nome
+        if usuario[0] == nome:
+            return 'Usuário já existente'
+    
+    cursor.execute(f"INSERT INTO usuarios (nome, senha, saldo) VALUES ('{nome}', '{senha}', 0)")
 
 eel.init('frontend')
-
 eel.start('index.html')
