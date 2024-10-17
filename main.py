a5009b3e7a1426ef7fa63355217eae063fa7d5f1
Anderson Gleidson dos Santos Carvalho <20221214010025@ifrn.local>
2024-10-17 17:52:05 -0300

    começando a editar usuários

--- main.py
+++ main.py
@@ -14,21 +14,36 @@ def login(nome,senha):
         if usuario[0] == nome and usuario[1] == senha:
             return 'loja.html'
     
-@eel.expose
-def criarConta(nome,senha):
+@eel.expose    
+def editarUsuario(nome='',senha='',novoNome='',novaSenha='',novoSaldo=''):
     if nome == '' or senha == '':
-        return 'Nome ou Senha vazios'
+        return 'Nome ou senha vazios'
     if len(nome) < 5 or len(senha) < 5:
-        return 'Nome ou Senha muito curtos'
+        return 'Nome ou senha muito curtos'
     
     cursor.execute('SELECT * FROM usuarios')
     listaUsuarios = cursor.fetchall()
 
+    usuarioEncontrado = None
     for usuario in listaUsuarios:#Não deixa criar usuários com mesmo nome
         if usuario[0] == nome:
+            usuarioEncontrado = usuario
+
+    if novoNome or novaSenha or novoSaldo:#Tentando editar
+        if usuarioEncontrado:
+            novoNome = usuarioEncontrado[0] if novoNome == '' else novoNome
+            novaSenha = usuarioEncontrado[1] if novoNome == '' else novaSenha
+            novoSaldo = usuarioEncontrado[2] if novoSaldo == '' else usuarioEncontrado[2] + float(novoSaldo)
+            cursor.execute(f'UPDATE usuarios SET nome={novoNome}, senha={novaSenha}, saldo={novoSaldo} WHERE nome={usuarioEncontrado[0]}')
+        else:
+            return 'Usuário não encontrado'
+        
+    elif nome and senha:#Tentando criar usuário
+        if usuarioEncontrado == None:
+            cursor.execute(f"INSERT INTO usuarios (nome, senha, saldo) VALUES ('{nome}', '{senha}', 0)")
+        else:
             return 'Usuário já existente'
-    
-    cursor.execute(f"INSERT INTO usuarios (nome, senha, saldo) VALUES ('{nome}', '{senha}', 0)")
+
 
 eel.init('frontend')
 eel.start('index.html')
